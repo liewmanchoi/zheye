0f6b2149be43f69b134ebaea106bb7c9b611211b
Liew Manchoi <liewmanchoi@gmail.com>
2019-05-13 15:43:22 +0800

    Initial commit

---
+++ src/main/java/com/liewmanchoi/zheye/ZheyeApplication.java
@@ -0,0 +1,13 @@
+package com.liewmanchoi.zheye;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZheyeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZheyeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/liewmanchoi/zheye/ZheyeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.liewmanchoi.zheye;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZheyeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
