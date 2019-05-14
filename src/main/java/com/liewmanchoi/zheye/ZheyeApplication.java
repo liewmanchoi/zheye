49745cd6baac9164c6ca5891ac4a93c247869c2a
Liew Manchoi <liewmanchoi@gmail.com>
2019-05-14 16:47:58 +0800

    初始化设置
    Signed-off-by: Liew Manchoi <liewmanchoi@gmail.com>

--- src/main/java/com/liewmanchoi/zheye/ZheyeApplication.java
+++ src/main/java/com/liewmanchoi/zheye/ZheyeApplication.java
@@ -2,12 +2,13 @@ package com.liewmanchoi.zheye;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 public class ZheyeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ZheyeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ZheyeApplication.class, args);
+    }
 
 }
