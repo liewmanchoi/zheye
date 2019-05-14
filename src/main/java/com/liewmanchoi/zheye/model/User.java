9d43f602ae1a09835a0833f19ddf84d5b12b667c
Liew Manchoi <liewmanchoi@gmail.com>
2019-05-14 16:52:57 +0800

    feat(model): User
    Signed-off-by: Liew Manchoi <liewmanchoi@gmail.com>

---
+++ src/main/java/com/liewmanchoi/zheye/model/User.java
@@ -0,0 +1,18 @@
+package com.liewmanchoi.zheye.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author wangsheng
+ * @date 2019/5/14
+ */
+@Getter
+@Setter
+public class User {
+    private int id;
+    private String name;
+    private String password;
+    private String salt;
+    private String avatarUrl;
+}
