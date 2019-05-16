50f4896d258d4e5deac00dcf7791ff809d502759
Liew Manchoi <liewmanchoi@gmail.com>
2019-05-16 17:10:44 +0800

    feat(Controller): 注册功能
    Signed-off-by: Liew Manchoi <liewmanchoi@gmail.com>

--- src/main/java/com/liewmanchoi/zheye/service/UserService.java
+++ src/main/java/com/liewmanchoi/zheye/service/UserService.java
@@ -1,10 +1,16 @@
 package com.liewmanchoi.zheye.service;
 
+import com.liewmanchoi.zheye.dao.LoginTicketDAO;
 import com.liewmanchoi.zheye.dao.UserDAO;
+import com.liewmanchoi.zheye.model.LoginTicket;
 import com.liewmanchoi.zheye.model.User;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.DigestUtils;
+
+import java.util.*;
 
 /**
  * @author wangsheng
@@ -16,7 +22,64 @@ public class UserService {
     @Autowired
     private UserDAO userDAO;
 
+    @Autowired
+    private LoginTicketDAO loginTicketDAO;
+
     public User getUser(int id) {
         return userDAO.selectById(id);
+    }
+
+    /**
+     * 注册新用户
+     *
+     * @param username 用户名
+     * @param password 用户密码
+     * @return java.util.Map<java.lang.String, java.lang.Object>
+     * @date 2019/5/16
+     */
+    public Map<String, Object> register(String username, String password) {
+        Map<String, Object> map = new HashMap<>();
+        if (StringUtils.isBlank(username)) {
+            map.put("msg", "用户名不能为空");
+            return map;
+        }
+
+        if (StringUtils.isBlank(password)) {
+            map.put("msg", "密码不能为空");
+            return map;
+        }
+
+        User user = userDAO.selectByName(username);
+        if (user != null) {
+            map.put("msg", "用户名已经被注册");
+            return map;
+        }
+
+        // 设置密码，对密码加盐后，存储MD5值
+        user = new User();
+        user.setName(username);
+        user.setSalt(UUID.randomUUID().toString().substring(0, 6));
+        String avatarUrl = String.format("../images/avatars/%d.jpg", new Random().nextInt(11));
+        user.setAvatarUrl(avatarUrl);
+        String saltedPassword = password + user.getSalt();
+        user.setPassword(DigestUtils.md5DigestAsHex(saltedPassword.getBytes()));
+        userDAO.addUser(user);
+
+        // 账户注册成功后，自动设置token
+        String ticket = addLoginTicket(user.getId());
+        map.put("ticket", ticket);
+        return map;
+    }
+
+    private String addLoginTicket(int userId) {
+        LoginTicket loginTicket = new LoginTicket();
+        loginTicket.setUserId(userId);
+        Date date = new Date();
+        date.setTime(date.getTime() + 1000 * 3600 * 24);
+        loginTicket.setExpired(date);
+        loginTicket.setStatus(0);
+        loginTicket.setTicket(UUID.randomUUID().toString().replaceAll("-", ""));
+
+        return loginTicket.getTicket();
     }
 }
