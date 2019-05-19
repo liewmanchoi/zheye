26d25d776cee387474eb80c3c891ccbdcf76f632
Liew Manchoi <liewmanchoi@gmail.com>
2019-05-19 11:30:56 +0800

    feat(Model): HostHolder
    标记当前会话中已登陆的用户
    
    Signed-off-by: Liew Manchoi <liewmanchoi@gmail.com>

---
+++ src/main/java/com/liewmanchoi/zheye/model/HostHolder.java
@@ -0,0 +1,24 @@
+package com.liewmanchoi.zheye.model;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author wangsheng
+ * @date 2019/5/19
+ */
+@Component
+public class HostHolder {
+    private static ThreadLocal<User> userThreadLocal = new ThreadLocal<>();
+
+    public User getUser() {
+        return userThreadLocal.get();
+    }
+
+    public void setUser(User user) {
+        userThreadLocal.set(user);
+    }
+
+    public void clear() {
+        userThreadLocal.remove();
+    }
+}
