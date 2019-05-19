0baf9dfe155df4e59abb3c2ebc731490283ff058
Liew Manchoi <liewmanchoi@gmail.com>
2019-05-19 12:17:25 +0800

    refactor(Controller): 优化LogAspect判断条件
    Signed-off-by: Liew Manchoi <liewmanchoi@gmail.com>

--- src/main/java/com/liewmanchoi/zheye/aspect/LogAspect.java
+++ src/main/java/com/liewmanchoi/zheye/aspect/LogAspect.java
@@ -22,6 +22,9 @@ public class LogAspect {
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append("args: ");
         for (Object arg : joinPoint.getArgs()) {
+            if (arg == null) {
+                break;
+            }
             stringBuilder.append(arg.toString()).append("|");
         }
         log.info("before method: " + stringBuilder.toString());
