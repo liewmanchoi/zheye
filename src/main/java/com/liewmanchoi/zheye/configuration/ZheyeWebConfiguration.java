3e8a1605e3555c33b6f7d0575ffd83ac59f0bfa5
Liew Manchoi <liewmanchoi@gmail.com>
2019-05-19 15:16:42 +0800

    feat(Controller): 拦截/reglogin请求
    Signed-off-by: Liew Manchoi <liewmanchoi@gmail.com>

--- src/main/java/com/liewmanchoi/zheye/configuration/ZheyeWebConfiguration.java
+++ src/main/java/com/liewmanchoi/zheye/configuration/ZheyeWebConfiguration.java
@@ -1,9 +1,10 @@
 package com.liewmanchoi.zheye.configuration;
 
+import com.liewmanchoi.zheye.interceptor.LoginRequiredInterceptor;
 import com.liewmanchoi.zheye.interceptor.PassportInterceptor;
+import com.liewmanchoi.zheye.interceptor.RegLoginInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,13 +13,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * @date 2019/5/19
  */
 @Configuration
-@EnableWebMvc
 public class ZheyeWebConfiguration implements WebMvcConfigurer {
     @Autowired
     PassportInterceptor passportInterceptor;
 
+    @Autowired
+    LoginRequiredInterceptor loginRequiredInterceptor;
+
+    @Autowired
+    RegLoginInterceptor regLoginInterceptor;
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(passportInterceptor);
+        registry.addInterceptor(loginRequiredInterceptor).addPathPatterns("/user/**");
+        registry.addInterceptor(regLoginInterceptor).addPathPatterns("/reglogin");
     }
 }
