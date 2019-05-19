baa17f168893bcd50a24618bdd782de22edb056d
Liew Manchoi <liewmanchoi@gmail.com>
2019-05-19 19:06:44 +0800

    feat(Service): QuestionService
    Signed-off-by: Liew Manchoi <liewmanchoi@gmail.com>

--- src/main/java/com/liewmanchoi/zheye/service/QuestionService.java
+++ src/main/java/com/liewmanchoi/zheye/service/QuestionService.java
@@ -5,6 +5,7 @@ import com.liewmanchoi.zheye.model.Question;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.util.HtmlUtils;
 
 import java.util.List;
 
@@ -20,5 +21,21 @@ public class QuestionService {
 
     public List<Question> getLatestQuestions(int userId, int offset, int limit) {
         return questionDAO.selectLatestQuestions(userId, offset, limit);
+    }
+
+    public Question getById(int id) {
+        return questionDAO.getById(id);
+    }
+
+    public int updateCommentCount(int id, int commentCount) {
+        return questionDAO.updateCommentCount(id, commentCount);
+    }
+
+    public int addQuestion(Question question) {
+        // 过滤脚本
+        question.setTitle(HtmlUtils.htmlEscape(question.getTitle()));
+        question.setContent(HtmlUtils.htmlEscape(question.getContent()));
+
+        return questionDAO.addQuestion(question) > 0 ? question.getId() : 0;
     }
 }
