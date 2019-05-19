60aceac14bf449f2ce44e241404624f02b121af9
Liew Manchoi <liewmanchoi@gmail.com>
2019-05-19 19:09:18 +0800

    feat(Utils): JSON工具类
    Signed-off-by: Liew Manchoi <liewmanchoi@gmail.com>

---
+++ src/main/java/com/liewmanchoi/zheye/utils/JSONUtils.java
@@ -0,0 +1,32 @@
+package com.liewmanchoi.zheye.utils;
+
+import com.alibaba.fastjson.JSONObject;
+
+import java.util.Map;
+
+/**
+ * @author wangsheng
+ * @date 2019/5/19
+ */
+public class JSONUtils {
+    public static String getJSONString(int code) {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("code", code);
+        return jsonObject.toJSONString();
+    }
+
+    public static String getJSONString(int code, String msg) {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("code", code);
+        jsonObject.put("msg", msg);
+        return jsonObject.toJSONString();
+    }
+
+    public static String getJSONString(int code, Map<String, Object> map) {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("code", code);
+        map.forEach(jsonObject::put);
+
+        return jsonObject.toJSONString();
+    }
+}
