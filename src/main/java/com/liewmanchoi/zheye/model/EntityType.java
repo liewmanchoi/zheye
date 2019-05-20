2bfd5ead18fd131df160d8ce9303a1b6d9f8cb19
Liew Manchoi <liewmanchoi@gmail.com>
2019-05-20 09:55:14 +0800

    feat(Model): EntityType
    表示实体类型：
    1-问题
    2-评论
    
    Signed-off-by: Liew Manchoi <liewmanchoi@gmail.com>

---
+++ src/main/java/com/liewmanchoi/zheye/model/EntityType.java
@@ -0,0 +1,10 @@
+package com.liewmanchoi.zheye.model;
+
+/**
+ * @author wangsheng
+ * @date 2019/5/20
+ */
+public class EntityType {
+    public static final int QUESTION = 1;
+    public static final int COMMENT = 2;
+}
