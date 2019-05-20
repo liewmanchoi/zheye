4cb9dd88349adecf4c6e88feddf7b8fe2b984971
Liew Manchoi <liewmanchoi@gmail.com>
2019-05-20 17:07:52 +0800

    feat(Service): MessageService
    Signed-off-by: Liew Manchoi <liewmanchoi@gmail.com>

---
+++ src/main/java/com/liewmanchoi/zheye/service/MessageService.java
@@ -0,0 +1,34 @@
+package com.liewmanchoi.zheye.service;
+
+import com.liewmanchoi.zheye.dao.MessageDAO;
+import com.liewmanchoi.zheye.model.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @author wangsheng
+ * @date 2019/5/20
+ */
+@Service
+public class MessageService {
+    @Autowired
+    MessageDAO messageDAO;
+
+    public int addMessage(Message message) {
+        return messageDAO.addMessage(message);
+    }
+
+    public List<Message> getConversationDetail(String conversationId, int offset, int limit) {
+        return messageDAO.getConversationDetail(conversationId, offset, limit);
+    }
+
+    public List<Message> getConversationList(int userId, int offset, int limit) {
+        return messageDAO.getConversationList(userId, offset, limit);
+    }
+
+    public int getConversationUnreadCount(int userId, String conversationId) {
+        return messageDAO.getConversationUnreadCount(userId, conversationId);
+    }
+}
