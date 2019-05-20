0d81aef164f98d7307bb6ff7cfaf304185521884
Liew Manchoi <liewmanchoi@gmail.com>
2019-05-20 15:47:25 +0800

    feat(Model): Message
    Signed-off-by: Liew Manchoi <liewmanchoi@gmail.com>

---
+++ src/main/java/com/liewmanchoi/zheye/model/Message.java
@@ -0,0 +1,29 @@
+package com.liewmanchoi.zheye.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+/**
+ * @author wangsheng
+ * @date 2019/5/20
+ */
+@Getter
+@Setter
+public class Message {
+    private int id;
+    private int fromId;
+    private int toId;
+    private String content;
+    private Date createdDate;
+    private int hasRead;
+    private String conversationId;
+
+    public String getConversationId() {
+        if (fromId < toId) {
+            return String.format("%d_%d", fromId, toId);
+        }
+        return String.format("%d_%d", toId, fromId);
+    }
+}
