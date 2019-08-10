2a377270bb3c10b6e5928a8f95684ce688412068
Liew Manchoi <liewmanchoi@gmail.com>
2019-08-10 16:51:18 +0800

    feat(config): RedisConfig
    Redis配置类

---
+++ src/main/java/com/liewmanchoi/zheye/config/RedisConfig.java
@@ -0,0 +1,50 @@
+package com.liewmanchoi.zheye.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SetOperations;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import redis.clients.jedis.JedisPoolConfig;
+
+/**
+ * @author wangsheng
+ * @date 2019/8/10
+ */
+@Configuration
+public class RedisConfig {
+  @Bean
+  public RedisConnectionFactory redisConnectionFactory() {
+    JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+    jedisPoolConfig.setMaxTotal(5);
+    jedisPoolConfig.setTestOnBorrow(true);
+    jedisPoolConfig.setTestOnReturn(true);
+
+    JedisConnectionFactory connectionFactory = new JedisConnectionFactory();
+    connectionFactory.setPoolConfig(jedisPoolConfig);
+    connectionFactory.setUsePool(true);
+    connectionFactory.setHostName("192.168.29.132");
+    connectionFactory.setPort(6379);
+
+    return connectionFactory;
+  }
+
+  @Bean(name = "likeServiceTemplate")
+  public RedisTemplate<String, Integer> likeServiceTemplate() {
+    RedisConnectionFactory factory = redisConnectionFactory();
+    RedisTemplate<String, Integer> redisTemplate = new RedisTemplate<>();
+    redisTemplate.setConnectionFactory(factory);
+    redisTemplate.setKeySerializer(new StringRedisSerializer());
+    redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(Integer.class));
+    return redisTemplate;
+  }
+
+  @Bean(name = "likeServiceOperations")
+  public SetOperations<String, Integer> likeServiceOperations() {
+    RedisTemplate<String, Integer> redisTemplate = likeServiceTemplate();
+    return redisTemplate.opsForSet();
+  }
+}
---
+++ src/main/java/com/liewmanchoi/zheye/utils/RedisKeyUtil.java
@@ -0,0 +1,20 @@
+package com.liewmanchoi.zheye.utils;
+
+/**
+ * @author wangsheng
+ * @date 2019/8/10
+ */
+public class RedisKeyUtil {
+  private static final String SPLIT = ":";
+  private static final String APP = "ZhEYE";
+  private static final String BIZ_LIKE = "LIKE";
+  private static final String BIZ_DISLIKE = "DISLIKE";
+
+  public static String getLikeKey(int entityType, int entityId) {
+    return APP + SPLIT + BIZ_LIKE + SPLIT + entityType + SPLIT + entityId;
+  }
+
+  public static String getDislikeKey(int entityType, int entityId) {
+    return APP + SPLIT + BIZ_DISLIKE + SPLIT + entityType + SPLIT + entityId;
+  }
+}
