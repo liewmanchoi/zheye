6cc08364d4c0d465b241373a8e499ddaf62400d1
Liew Manchoi <liewmanchoi@gmail.com>
2019-08-12 12:06:18 +0800

    feat(Service): FollowService

--- src/main/java/com/liewmanchoi/zheye/config/RedisConfig.java
+++ src/main/java/com/liewmanchoi/zheye/config/RedisConfig.java
@@ -6,8 +6,10 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.SetOperations;
+import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import redis.clients.jedis.JedisPoolConfig;
 
 /**
@@ -15,6 +17,7 @@ import redis.clients.jedis.JedisPoolConfig;
  * @date 2019/8/10
  */
 @Configuration
+@EnableTransactionManagement
 public class RedisConfig {
   @Bean
   public RedisConnectionFactory redisConnectionFactory() {
@@ -32,13 +35,15 @@ public class RedisConfig {
     return connectionFactory;
   }
 
-  @Bean(name = "likeServiceTemplate")
+  @Bean(name = "stringIntegerTemplate")
   public RedisTemplate<String, Integer> likeServiceTemplate() {
     RedisConnectionFactory factory = redisConnectionFactory();
     RedisTemplate<String, Integer> redisTemplate = new RedisTemplate<>();
     redisTemplate.setConnectionFactory(factory);
     redisTemplate.setKeySerializer(new StringRedisSerializer());
     redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(Integer.class));
+    // 开启事务支持
+    redisTemplate.setEnableTransactionSupport(true);
     return redisTemplate;
   }
 
@@ -46,5 +51,11 @@ public class RedisConfig {
   public SetOperations<String, Integer> likeServiceOperations() {
     RedisTemplate<String, Integer> redisTemplate = likeServiceTemplate();
     return redisTemplate.opsForSet();
+  }
+
+  @Bean(name = "followerServiceOperations")
+  public ZSetOperations<String, Integer> followerServiceOperations() {
+    RedisTemplate<String, Integer> redisTemplate = likeServiceTemplate();
+    return redisTemplate.opsForZSet();
   }
 }
