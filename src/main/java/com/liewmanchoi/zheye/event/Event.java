22f6382aca8ad914874e98d0a32d2aa2fd72e65a
Liew Manchoi <liewmanchoi@gmail.com>
2019-08-13 21:55:09 +0800

    fix(event): Event

--- src/main/java/com/liewmanchoi/zheye/event/Event.java
+++ src/main/java/com/liewmanchoi/zheye/event/Event.java
@@ -11,16 +11,23 @@ import lombok.Setter;
  */
 @Getter
 public class Event {
-  /** 时间类型 */
-  private EventType eventType;
+  /** 事件（操作）类型 */
+  private EventType operation;
   /** 事件触发者 */
   private int actorId;
-
+  /** （影响到的）资源的类型 */
   private int entityType;
+  /** （影响到的）资源的id */
   private int entityId;
+  /** 资源创建者（仅用于问题、回答、评论等） */
   private int entityOwnerId;
   /** 附加字段 */
   @Setter private Map<String, Object> attachments = new HashMap<>();
+
+  public Event setOperation(EventType operation) {
+    this.operation = operation;
+    return this;
+  }
 
   public Event setActorId(int actorId) {
     this.actorId = actorId;
